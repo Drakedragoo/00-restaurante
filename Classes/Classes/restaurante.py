cbb76dc1548b8fdeb10d1278ede31159c0472ab9
Drakedragoo <163123866+Drakedragoo@users.noreply.github.com>
2024-05-24 11:22:15 -0300

    Add files via upload

--- Classes/Classes/restaurante.py
+++ Classes/Classes/restaurante.py
@@ -1,4 +1,4 @@
-from avaliacao import Avaliacao
+from Classes.avaliacao import Avaliacao
 
 class Restaurante:
     restaurantes = []
@@ -17,7 +17,7 @@ class Restaurante:
     def listar_restaurantes(cls):
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Statu'.ljust(25)}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {str(restaurante.media_avaliacoes.lhust(25))} | {restaurante.ativo}')
 
     @property
     def _ativo(self):
@@ -26,6 +26,16 @@ class Restaurante:
     def alternar_estado(self):
         self._ativo = not self._ativo
 
-        def receber_avaliacao(self,cliente,nota):
-            avaliacao = Avaliacao(cliente,nota)
-            self._avaliacao.append(avaliacao)
+    def receber_avaliacao(self,cliente,nota):
+        avaliacao = Avaliacao(cliente,nota)
+        self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = soma_das_notas / quantidade_de_notas
+        return media
+
